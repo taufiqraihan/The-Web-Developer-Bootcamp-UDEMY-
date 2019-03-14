cade3b7d6c012b3c408e10540ada0f14a7ce2608
MUHAMMAD TAUFIQ RAIHAN <taufiqraihan9mn@gmail.com>
2019-03-14 14:04:50 +0700

    Add files via upload

---
+++ The WEb Developer BootCamp/section 10 (Introduction to javascript)/01-JS-Stalker-Exercise/Script.js
@@ -0,0 +1,7 @@
+const firstName = prompt('what is yours first nama?');
+const lastName = prompt('what is yours last nama?');
+const age = prompt('what is your age?');
+const fullName = (firstName + ' ' + lastName);
+
+console.log('your name is ' + fullName);
+console.log('yout age is ' + age + 'years old');
---
+++ The WEb Developer BootCamp/section 10 (Introduction to javascript)/02-Age-Calculator-Exercise/Calculator.js
@@ -0,0 +1,3 @@
+var age = prompt('what is your age?');
+var days = age * 365 ;
+alert(age + ' adalah ' + days + ' hari' );
